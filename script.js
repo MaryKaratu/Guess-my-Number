1e5e85566da782a8006f9902c35fc36eda90ef3e
MaryKaratu <Karatumary85@gmail.com>
2024-10-09 17:08:54 +0100

    Refactoring

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ document.querySelector('.guess').value = 23;
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -15,49 +20,71 @@ document.querySelector('.check').addEventListener('click', function () {
 
   //when there is no guess
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔~No number';
+    // document.querySelector('.message').textContent = '⛔~No number';
+    displayMessage('⛔~No number');
   }
 
   //when player wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉Correct Number';
+    // document.querySelector('.message').textContent = '🎉Correct Number';
+    displayMessage('🎉Correct Number');
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347 ';
     document.querySelector('.number').style.width = '30rem';
-  }
 
-  //when guess is too high
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  }
-
-  //when guess is too low
-  else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📉Too low';
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? '📈Too high' : '📉Too low';
+      displayMessage(guess > secretNumber ? '📈Too high' : '📉Too low');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      // document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
+//   //when guess is too high
+//   else if (guess > secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '📈Too high';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+
+//   //when guess is too low
+//   else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector('.message').textContent = '📉Too low';
+//       score--;
+//       document.querySelector('.score').textContent = score;
+//     } else {
+//       document.querySelector('.message').textContent = 'You lost the game!';
+//       document.querySelector('.score').textContent = 0;
+//     }
+//   }
+// });
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing....';
+
+  // document.querySelector('.message').textContent = 'Start guessing....';
+  displayMessage('Start guessing....');
   document.querySelector('.number').textContent = '?';
-  document.querySelectorAll('.score').textContent = score;
+  document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222';
