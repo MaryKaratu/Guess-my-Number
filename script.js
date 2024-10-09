35cbfc2fb8726d8451d1f01df61926946e626260
MaryKaratu <Karatumary85@gmail.com>
2024-10-09 14:38:51 +0100

    manipulating CSS

--- script.js
+++ script.js
@@ -14,11 +14,22 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no guess
   if (!guess) {
     document.querySelector('.message').textContent = '⛔~No number';
-  } else if (guess === secretNumber) {
+  }
+
+  //when player wins
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number';
-  } else if (guess > secretNumber) {
+  }
+ 
+  document.querySelector('body').style.backgroundColor = '#60b347 '
+
+  document.querySelector('.number').style.width ='30rem'
+
+  //when guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too high';
       score--;
@@ -27,7 +38,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+
+  //when guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉Too low';
       score--;
